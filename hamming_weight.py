31e167c032c91ced1a15f781da2ac08385a25211
Michael <michaelmunson1@gmail.com>
2016-01-08 21:41:10 -0500

    comment up front describing optimal implementation for repeated use

--- hamming_weight.py
+++ hamming_weight.py
@@ -1,5 +1,9 @@
 import random
 
+# N.B.: If computation is done repeatedly, most efficient method would be to create a lookup table.
+# Might lookup 16 bits at a time (table of size 2^16 = 65536 entries); for inputs > 65535,
+# mod by powers of 2 ^ 16 and add results of lookups
+
 def hamming_weight_brute_force(n):
     """
     O(len(n))
@@ -23,11 +27,11 @@ def hamming_weight_smarter(n):
     return count
 
 
-for i in xrange(10):
-    next = random.randint(0, 1024)
-    brute = hamming_weight_brute_force(next)
-    smart = hamming_weight_smarter(next)
-    if brute != smart:
-        print 'brute_force for %d returns %d - should return %d' % (next, smart, brute)
-    else:
-        print 'result for %d is correct (%d)' % (next, smart)
+# for i in xrange(10):
+#     next = random.randint(0, 1024)
+#     brute = hamming_weight_brute_force(next)
+#     smart = hamming_weight_smarter(next)
+#     if brute != smart:
+#         print 'brute_force for %d returns %d - should return %d' % (next, smart, brute)
+#     else:
+#         print 'result for %d is correct (%d)' % (next, smart)
